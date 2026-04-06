607db934305c649495135e7a741498bb2552d040
Youssef-kadd <yousseefkaddech@gmail.com>
2026-04-06 01:04:48 +0100

    Complete template integration for Hirely onboarding module

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    #[Route('/admin/plans', name: 'app_admin_plans')]
+    public function plans(): Response
+    {
+        return $this->render('admin/plans.html.twig');
+    }
+
+    #[Route('/admin/tasks', name: 'app_admin_tasks')]
+    public function tasks(): Response
+    {
+        return $this->render('admin/tasks.html.twig');
+    }
+}
